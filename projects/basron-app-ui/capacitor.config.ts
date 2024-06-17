04e567715554efac8c4f0ed151fa32aaf23c93eb
Ajithkumar74 <70085441+Ajithkumar74@users.noreply.github.com>
2024-06-17 14:45:42 +0530

    first

--- projects/basron-app-ui/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.virtusa.app',
-  appName: 'basron-app-ui',
-  webDir: 'dist/basron-app-ui/browser'
-};
-
-export default config;
